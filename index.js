23f278d1c9c45a247ae565db77da8e40e9f522ad
nettenegri <nettenegri@gmail.com>
2022-12-03 11:51:57 -0700

    commit tree cccc848fb856c3f13b31a2152333ef796599c61a

---
+++ index.js
@@ -0,0 +1,9 @@
+const num1 = 2000;
+const num2 = 100;
+function divide(num1, num2) {
+  console.log(num1 / num2);
+  return num1 / num2;
+}
+
+const square = (x) => x * x;
+const add = (x, y) => x + y;
